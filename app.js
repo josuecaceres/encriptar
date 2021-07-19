de4cdf9c5d3806ef564cb132dfea79c0898a3355
Elvin J Cáceres <51128186+josuecaceres@users.noreply.github.com>
2021-07-18 20:58:18 -0600

    agregue la encriptacion

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ var indiceTemporal
 //Guardado de la info-------------------------------------------------------------------------------
 formRegistro.addEventListener('submit', Event => {
   Event.preventDefault()
-  var user = new FormData(formRegistro)
+  var user = new FormData()
   user.append('metodo', 'agregar')
+  user.append('nombre', encriptar(document.getElementById('inputNombre').value))
+  user.append('correo', encriptar(document.getElementById('inputCorreo').value))
+  user.append('contraseña', encriptar(document.getElementById('inputContraseña').value))
+  user.append('cargo', encriptar(document.getElementById('inputCargo').value))
 
   fetch("php/usuarios.php", {
       method: 'POST',
@@ -19,6 +23,7 @@ formRegistro.addEventListener('submit', Event => {
     }).then(res => res.json())
     .then(data => {
       console.log(data)
+      formRegistro.reset()
       getUsers()
     })
 })
@@ -73,10 +78,10 @@ function getUsers() {
 //editar
 function edit(index) {
   console.log(usuarios[index])
-  document.getElementById('inputNombre').value = usuarios[index].nombre
-  document.getElementById('inputCorreo').value = usuarios[index].correo
-  document.getElementById('inputContraseña').value = usuarios[index].password
-  document.getElementById('inputTipo').value = usuarios[index].cargo
+  document.getElementById('inputNombre').value = desEncriptar(usuarios[index].nombre)
+  document.getElementById('inputCorreo').value = desEncriptar(usuarios[index].correo)
+  document.getElementById('inputContraseña').value = desEncriptar(usuarios[index].password)
+  document.getElementById('inputCargo').value = desEncriptar(usuarios[index].cargo)
 
   indiceTemporal = index
   document.getElementById('btnRegistrar').style.display = 'none'
@@ -84,9 +89,14 @@ function edit(index) {
 }
 
 document.getElementById('btnActualizar').addEventListener('click', () => {
-  var data = new FormData(formRegistro)
+  var data = new FormData()
   data.append('metodo', 'editar')
   data.append('id', usuarios[indiceTemporal].id)
+  
+  data.append('nombre', encriptar(document.getElementById('inputNombre').value))
+  data.append('correo', encriptar(document.getElementById('inputCorreo').value))
+  data.append('contraseña', encriptar(document.getElementById('inputContraseña').value))
+  data.append('cargo', encriptar(document.getElementById('inputCargo').value))
 
   fetch("php/usuarios.php", {
       method: "POST",
@@ -125,3 +135,74 @@ document.getElementById('btnReset').addEventListener('click', () => {
   document.getElementById('btnRegistrar').style.display = 'inline'
   document.getElementById('btnActualizar').style.display = 'none'
 })
+
+//Funciones para encriptar y desencriptar
+function encriptar(cadena){
+  let cadenaDividida = cadena.split("")
+  let cadenaEncriptada = ''
+  cadenaDividida.forEach(letra => {
+    switch(letra.toLowerCase()){
+      case "m": letra = "0"; break;
+      case "u": letra = "1"; break;
+      case "r": letra = "2"; break;
+      case "c": letra = "3"; break;
+      case "i": letra = "4"; break;
+      case "e": letra = "5"; break;
+      case "l": letra = "6"; break;
+      case "a": letra = "7"; break;
+      case "g": letra = "8"; break;
+      case "o": letra = "9"; break;
+
+      case "0": letra = "m"; break;
+      case "1": letra = "u"; break;
+      case "2": letra = "r"; break;
+      case "3": letra = "c"; break;
+      case "4": letra = "i"; break;
+      case "5": letra = "e"; break;
+      case "6": letra = "l"; break;
+      case "7": letra = "a"; break;
+      case "8": letra = "g"; break;
+      case "9": letra = "o"; break;
+
+      case " ": letra = "*"; break;
+    }
+    cadenaEncriptada += letra
+  })
+  
+  return cadenaEncriptada
+}
+
+function desEncriptar(cadena){
+  let cadenaDividida = cadena.split("")
+  let cadenaDesEncriptada = ''
+  cadenaDividida.forEach(letra => {
+    switch(letra.toLowerCase()){
+      case "m": letra = "0"; break;
+      case "u": letra = "1"; break;
+      case "r": letra = "2"; break;
+      case "c": letra = "3"; break;
+      case "i": letra = "4"; break;
+      case "e": letra = "5"; break;
+      case "l": letra = "6"; break;
+      case "a": letra = "7"; break;
+      case "g": letra = "8"; break;
+      case "o": letra = "9"; break;
+
+      case "0": letra = "m"; break;
+      case "1": letra = "u"; break;
+      case "2": letra = "r"; break;
+      case "3": letra = "c"; break;
+      case "4": letra = "i"; break;
+      case "5": letra = "e"; break;
+      case "6": letra = "l"; break;
+      case "7": letra = "a"; break;
+      case "8": letra = "g"; break;
+      case "9": letra = "o"; break;
+
+      case " ": letra = "*"; break;
+    }
+    cadenaDesEncriptada += letra
+  })
+  
+  return cadenaDesEncriptada
+}
